288da02dd098226cfa6b9feedc00e94820567282
masud.chowdhury <mchowdhury.eng@gmail.com>
2022-02-04 11:38:27 +0000

    fix variable reference

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -48,7 +48,7 @@ resource "aws_autoscaling_group" "example" {
 
     content {
       key                 = tag.key
-      value               = var.value
+      value               = tag.value
       propagate_at_launch = true
     }
   }
